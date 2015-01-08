77feb40da73d179767fd27dfed892a345df62733
koduki <koduki@dummy.server.fake>
2015-01-09 04:19:24 +0900

    add regist page.

--- client/templates/main.js
+++ client/templates/main.js
@@ -4,3 +4,17 @@ Template.alarmsList.helpers({
     return Alarms.find({});
   }
 });
+
+Template.alarmsEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();//formのsubmit処理無効化
+
+    var alarm = {
+      name: $(e.target).find('[name=name]').val(),
+      time: $(e.target).find('[name=time]').val()
+    };
+
+    alarm._id = Alarms.insert(alarm);
+    Router.go('alarmsList', alarm);
+  }
+});
